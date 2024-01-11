85dd457cc0961aaf85c8d3dd0ceb29e3cdcaaff3
Balakrishnayempati <balakrishnayempati007@gmail.com>
2024-01-11 11:24:04 +0530

    jan

---
+++ jan1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int x,y,z;
+printf("enter x value:\n");
+scanf("%d\n",&x);
+printf("enter y value:\n");
+scanf("%d\n",&y);
+z=x+y;
+printf("z value is %d\n",z);
+return 0;
+}
+
+
+
+
